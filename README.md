3971ef2bde8e9b3bc9ffc42784742e74c9e6c339
indu <indira.indukuri@cgi.com>
2019-08-02 22:38:32 +0530

    Removed hardcoded data using @PropertySource

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-## Task7
+## Task8
 
-Global exception using Controller advice
+Remove all hardcoded data from the application code to application.properties
+
+a) by using @Value.     
+b) by using @PropertySource     
+c) by using Environment     
+d) by using @ConfigurationProperties
--- src/main/java/com/stackroute/seedData/ContextRefreshedListener.java
+++ src/main/java/com/stackroute/seedData/ContextRefreshedListener.java
@@ -2,13 +2,20 @@ package com.stackroute.seedData;
 
 import com.stackroute.domain.Track;
 import com.stackroute.repository.TrackRepository;
-import com.stackroute.service.TrackService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
+
+/**@PropertySource to read a properties file*/
+@PropertySource("classpath:application.properties")
+
+
 /**Context Refreshed Listener is used to execute the code after the Spring Boot application started*/
 public class ContextRefreshedListener implements ApplicationListener<ContextRefreshedEvent> {
 
@@ -22,16 +29,24 @@ public class ContextRefreshedListener implements ApplicationListener<ContextRefr
         this.trackRepository = trackRepository;
     }
 
+
+    /**Using Environment to get the property values*/
+    @Autowired
+    private Environment environment;
+
+
+    /**To get the property values*/
+    @Value("${id}")
+    int id;
+
+
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         /**Pre-fill the database whenever application starts*/
-        Track track1 = new Track(1, "track1", "comment1");
-        Track track2 = new Track(2, "track2", "comment2");
-        Track track3 = new Track(3, "track3", "comment3");
-        Track track4 = new Track(4, "track4", "comment4");
-        trackRepository.save(track1);
-        trackRepository.save(track2);
-        trackRepository.save(track3);
-        trackRepository.save(track4);
+        try {
+            trackRepository.save(new Track(id, environment.getProperty("trackName"), environment.getProperty("comments")));
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 }
