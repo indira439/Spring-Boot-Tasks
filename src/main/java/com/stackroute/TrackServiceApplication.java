895b09fc43c541777e2c7db8738d01c7ce82aa54
indu <indira.indukuri@cgi.com>
2019-08-02 13:59:57 +0530

    Generated API documentation using Swagger 2

--- src/main/java/com/stackroute/TrackServiceApplication.java
+++ src/main/java/com/stackroute/TrackServiceApplication.java
@@ -2,9 +2,14 @@ package com.stackroute;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-/**Enables Spring Boot auto configuration and component scanning.*/
+/**
+ * Enables Spring Boot auto config and component scanning.
+ */
 @SpringBootApplication
+/**Enable swagger support in the class*/
+@EnableSwagger2
 public class TrackServiceApplication {
 
     public static void main(String[] args) {
