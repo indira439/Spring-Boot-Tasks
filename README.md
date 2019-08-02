b3eead546f27b8b23be19e550302485f66000eae
indu <indira.indukuri@cgi.com>
2019-08-02 17:40:00 +0530

    Updated README

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-## Task5
-Created custom exceptions TrackNotFoundException, TrackAlreadyExistsException in a
-com.stackroute.exceptions package.
+## Task6
+
+Running Logic on Startup in Spring. Created seed data to pre-fill the database with movie
+information whenever the application starts. Using both approaches:
+
+ Approach 1: ApplicationListener<ContextRefreshedEvent>
+ Approach 2: CommandLineRunner (Find out how it differs from ApplicationRunner)
--- src/main/java/com/stackroute/seedData/ContextRefreshedListener.java
+++ src/main/java/com/stackroute/seedData/ContextRefreshedListener.java
@@ -9,10 +9,14 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 @Component
+/**Context Refreshed Listener is used to execute the code after the Spring Boot application started*/
 public class ContextRefreshedListener implements ApplicationListener<ContextRefreshedEvent> {
 
     private TrackRepository trackRepository;
 
+    /**
+     * Use constructor based DI to inject TrackRepository here
+     */
     @Autowired
     private ContextRefreshedListener(TrackRepository trackRepository) {
         this.trackRepository = trackRepository;
@@ -20,11 +24,11 @@ public class ContextRefreshedListener implements ApplicationListener<ContextRefr
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-
-        Track track1 = new Track(1,"track1","comment1");
-        Track track2 = new Track(2,"track2","comment2");
-        Track track3 = new Track(3,"track3","comment3");
-        Track track4 = new Track(4,"track4","comment4");
+        /**Pre-fill the database whenever application starts*/
+        Track track1 = new Track(1, "track1", "comment1");
+        Track track2 = new Track(2, "track2", "comment2");
+        Track track3 = new Track(3, "track3", "comment3");
+        Track track4 = new Track(4, "track4", "comment4");
         trackRepository.save(track1);
         trackRepository.save(track2);
         trackRepository.save(track3);
